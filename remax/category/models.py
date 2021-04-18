d0d801d29e5b816ae9d8e923c9bdb4c7a079a523
Mohammed Hamdy <mohammedayada1998@gmail.com>
2021-04-18 15:54:32 +0200

    update models and remove slug from category

--- remax/category/models.py
+++ remax/category/models.py
@@ -3,12 +3,10 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
     parent = models.ForeignKey('self', blank=True, null=True, related_name='child', on_delete=models.CASCADE)
 
 
     class Meta:
-        unique_together = ('slug', 'parent',)
         verbose_name_plural = "Categories"
 
     def __str__(self):
