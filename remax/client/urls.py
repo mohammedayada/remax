431e9f4deb2c8c58630bf45dba5a201baee5876e
Mohammed Hamdy <mohammedayada1998@gmail.com>
2021-04-22 05:45:12 +0200

    Add all API for client app

--- remax/client/urls.py
+++ remax/client/urls.py
@@ -1,13 +1,34 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ClientViewSet, ClientLocationViewSet, ClientPhoneViewSet
-# Create a router and register our viewsets with it.
-router = DefaultRouter()
-router.register(r'clients', ClientViewSet, basename='clients')
-router.register(r'client-location', ClientLocationViewSet, basename='client-location')
-router.register(r'client-phone', ClientPhoneViewSet, basename='client-phone')
+from .views import (
+    ClientGetList,
+    ClientPostList,
+    ClientDetail,
+    ClientPhoneGetList,
+    ClientPhonePostList,
+    ClientPhoneDetail,
+    GetAllClientPhonesById,
+    ClientLocationGetList,
+    ClientLocationPostList,
+    ClientLocationDetail,
+    GetAllClientLocationsById,
+)
+
+
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('getclients/', ClientGetList.as_view()),
+    path('postclients/', ClientPostList.as_view()),
+    path('clients/<int:pk>/', ClientDetail.as_view()),
+
+    path('getclientphones/', ClientPhoneGetList.as_view()),
+    path('postclientphones/', ClientPhonePostList.as_view()),
+    path('clientphones/<int:pk>/', ClientPhoneDetail.as_view()),
+    path('clientphonesbyid/<int:pk>/', GetAllClientPhonesById.as_view()),
+
+    path('getclientlocations/', ClientLocationGetList.as_view()),
+    path('postclientlocations/', ClientLocationPostList.as_view()),
+    path('clientlocations/<int:pk>/', ClientLocationDetail.as_view()),
+    path('clientlocationsbyid/<int:pk>/', GetAllClientLocationsById.as_view()),
 ]
--- remax/client/views.py
+++ remax/client/views.py
@@ -9,19 +9,218 @@ from .serializer import (
 )
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils.decorators import method_decorator
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 
-class ClientViewSet(viewsets.ModelViewSet):
-    permission_classes = (AllowAny,)
-    serializer_class = ClientSerializer
-    queryset = Client.objects.all()
+# To get all Clients
+class ClientGetList(APIView):
+    """
+    List all Clients.
+    """
+    permission_classes = [IsAuthenticated]
 
-class ClientPhoneViewSet(viewsets.ModelViewSet):
-    permission_classes = (AllowAny,)
-    serializer_class = ClientPhoneSerializer
-    queryset = Client_Phone.objects.all()
+    def get(self, request, format=None):
+        clients = Client.objects.all()
+        serializer = ClientSerializer(clients, many=True)
+        return Response(serializer.data)
 
-class ClientLocationViewSet(viewsets.ModelViewSet):
-    permission_classes = (AllowAny,)
-    serializer_class = ClientLocationSerializer
-    queryset = Client_Location.objects.all()
+
+# To post Clients
+class ClientPostList(APIView):
+    """
+    Create a new Client.
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        serializer = ClientSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# To Retrieve, update or delete a Client instance.
+class ClientDetail(APIView):
+    """
+    Retrieve, update or delete a Client instance.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Client.objects.get(pk=pk)
+        except Client.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        client = self.get_object(pk)
+        serializer = ClientSerializer(client)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        client = self.get_object(pk)
+        serializer = ClientSerializer(client, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        client = self.get_object(pk)
+        client.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# To get all Client phone
+class ClientPhoneGetList(APIView):
+    """
+    List all Client Phone.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        client_phone = Client_Phone.objects.all()
+        serializer = ClientPhoneSerializer(client_phone, many=True)
+        return Response(serializer.data)
+
+
+# To post Client Phone
+class ClientPhonePostList(APIView):
+    """
+    Create a new Client Phone.
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        serializer = ClientPhoneSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# To Retrieve, update or delete a Client Phone instance.
+class ClientPhoneDetail(APIView):
+    """
+    Retrieve, update or delete a Client Phone instance.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Client_Phone.objects.get(pk=pk)
+        except Client_Phone.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        client_phone = self.get_object(pk)
+        serializer = ClientPhoneSerializer(client_phone)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        client_phone = self.get_object(pk)
+        serializer = ClientPhoneSerializer(client_phone, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        client_phone = self.get_object(pk)
+        client_phone.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# To get all Client phone by client id
+class GetAllClientPhonesById(APIView):
+    """
+    List all Client Phone.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk, format=None):
+        client = Client.objects.filter(pk=pk).first()
+        client_phone = Client_Phone.objects.filter(client=client)
+        serializer = ClientPhoneSerializer(client_phone, many=True)
+        return Response(serializer.data)
+
+
+# To get all Client Location
+class ClientLocationGetList(APIView):
+    """
+    List all Client Location.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        client_location = Client_Location.objects.all()
+        serializer = ClientLocationSerializer(client_location, many=True)
+        return Response(serializer.data)
+
+
+# To post Client Location
+class ClientLocationPostList(APIView):
+    """
+    Create a new Client Location.
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        serializer = ClientLocationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# To Retrieve, update or delete a Client Location instance.
+class ClientLocationDetail(APIView):
+    """
+    Retrieve, update or delete a Client Location instance.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Client_Location.objects.get(pk=pk)
+        except Client_Location.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        client_location = self.get_object(pk)
+        serializer = ClientLocationSerializer(client_location)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        client_location = self.get_object(pk)
+        serializer = ClientLocationSerializer(client_location, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        client_location = self.get_object(pk)
+        client_location.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# To get all Client Location by client id
+class GetAllClientLocationsById(APIView):
+    """
+    List all Client Location.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk, format=None):
+        client = Client.objects.filter(pk=pk).first()
+        client_location = Client_Location.objects.filter(client=client)
+        serializer = ClientLocationSerializer(client_location, many=True)
+        return Response(serializer.data)
